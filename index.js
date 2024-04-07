485b6171a2013ebac45422c0048627ca8681e215
Mykhan99 <160237773+Mykhan99@users.noreply.github.com>
2024-04-07 12:59:51 -0700

    My first upload

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let toDo = [];
+let condition = true;
+while (condition) {
+    let taskAdd = await inquirer.prompt([{
+            message: "What is your to do list?",
+            type: "input",
+            name: "toDoList"
+        },
+        {
+            message: "You want to add more?",
+            type: "confirm",
+            name: "addMore",
+            default: "false"
+        }
+    ]);
+    toDo.push(taskAdd.toDoList);
+    condition = taskAdd.addMore;
+}
+toDo = toDo.concat();
+console.log(toDo);
